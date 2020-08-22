940b56b75377618cf9cf091a865b77522d7bbf6b
Bruno Honda <bruno.honda@4linux.com.br>
2020-08-21 23:37:10 -0300

    ESLint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 import express, { Request, Response } from 'express';
 
 const app = express();
 
-app.get('/', (request: Request, response: Response) => {
-    return response.json({
-        message: 'Hello world!!!'
-    });
-})
+app.get(
+    '/',
+    (request: Request, response: Response) => response.json({
+      message: 'Hello world!!!'
+    })
+);
 
 app.listen(3000);
